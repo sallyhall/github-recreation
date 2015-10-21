453cbd1bd72e96c955e276265b1f50fa09482842
Sally Kingston <sally.a.kingston@gmail.com>
2015-10-21 19:22:22 -0400

    data, and a bit more formatting

--- main.js
+++ main.js
@@ -12,8 +12,6 @@ var following = profile.following;
 var starredNum = starred.length;
 
 
-
-
 //Fill header info
 $("title").text(username + " (" + name + ")");
 $(".small-avatar").html("<img src = " + avatarURL + "/>");
@@ -26,13 +24,91 @@ $(".company").text(company);
 $(".location").text(myLocation);
 $(".email").text(email);
 $(".blog").text(blog);
+$(".joinDate").text(joinedDate);
 
 repos = _.sortBy(repos,function(repo){
   var dt = new Date(repo.updated_at);
   return -dt;
 });
 
+// var repoTemplate = _.template("<h3 class = \"title\"<a href=\"  <%- repo.url %-> \"> <%- repo.name %></a></h3>")
+
+
+
+//add all the repos
 _.each(repos, function (repo) {
-  $("main").append("<p>"+repo.name + "updated " + moment(repo.updated_at).fromNow()+"</p>");
-  $("main").append("<p>"+repo.language + " watchers: " +repo.stargazers_count + " forks: " +repo.forks_count+"</p>");
+  $(".repos").append("<h3 class = \"repo_title\"> <a href= \""
+                      + repo.html_url
+                      + "\">"
+                      + repo.name
+                      + "</a></h3><p class = \"repo_data\">Updated "
+                      + moment(repo.updated_at).fromNow()
+                      + "</p><div class = \"repo_stats\">"
+                      + repo.language
+                      + "<a href= \""
+                      + repo.html_url
+                      + "/stargazers\"><span class=\"octicon octicon-star\"></span>"
+                      + repo.stargazers_count
+                      + "</a><a href= \""
+                      + repo.html_url
+                      + "/network\"><span class=\"octicon octicon-git-branch\"></span>"
+                      + repo.forks_count
+                      + "</a>");
+});
+
+var activityText="";
+//this isn't done
+_.each(events, function (currEvent) {
+  if(currEvent.type==="PushEvent"){
+    activityText = "<span class=\"mega-octicon octicon-git-commit\"></span><div class = \"time\">"
+                    + moment(currEvent.created_at).fromNow()
+                    + "</div><div class = \"title\">"
+                    + currEvent.actor.login
+                    + " pushed to "
+                    + currEvent.payload.ref.split("/")[2]
+                    + " at "
+                    + currEvent.repo.name
+                    + "</div>"
+                    + "<div class = \"details\">"
+                    //+ big avatar
+                    //+ little avatar
+                    + currEvent.payload.head.substring(0,6)
+                    + " "
+                    + currEvent.payload.commits[0].message;
+  }else if (currEvent.payload.ref_type === "repository"){
+    activityText = "<span class=\"octicon octicon-repo\"></span>"
+                    + currEvent.actor.login
+                    + " created repository "
+                    + currEvent.repo.name
+                    + moment(currEvent.created_at).fromNow()
+                    +"<br>";
+  }else{
+    activityText = "<span class=\"octicon octicon-git-branch\"></span>"
+                    + currEvent.actor.login
+                    + " created branch "
+                    + currEvent.payload.ref.split("/")[2]
+                    + " at "
+                    + currEvent.repo.name
+                    + moment(currEvent.created_at).fromNow()
+                    +"<br>";
+  }
+  $(".activity").append(activityText);
+});
+
+
+$(".repoTab").click(function (event) {
+  event.preventDefault();
+  $(".repos").css("display","block");
+  $(".repos").siblings().css("display","none");
+});
+$(".contrTab").click(function (event) {
+  event.preventDefault();
+  $(".contributions").css("display","block");
+  $(".contributions").siblings().css("display","none");
+});
+
+$(".activTab").click(function (event) {
+  event.preventDefault();
+  $(".activity").css("display","block");
+  $(".activity").siblings().css("display","none");
 });
