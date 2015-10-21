96917438d357946ee1ddf74910095d4d476804f9
Sally Kingston <sally.a.kingston@gmail.com>
2015-10-20 23:57:45 -0400

    most of the data

--- main.js
+++ main.js
@@ -26,3 +26,13 @@ $(".company").text(company);
 $(".location").text(myLocation);
 $(".email").text(email);
 $(".blog").text(blog);
+
+repos = _.sortBy(repos,function(repo){
+  var dt = new Date(repo.updated_at);
+  return -dt;
+});
+
+_.each(repos, function (repo) {
+  $("main").append("<p>"+repo.name + "updated " + moment(repo.updated_at).fromNow()+"</p>");
+  $("main").append("<p>"+repo.language + " watchers: " +repo.stargazers_count + " forks: " +repo.forks_count+"</p>");
+});
