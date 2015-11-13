2394e09f0e3eb896e25422296e1ace9c6c6df9e3
Sally Kingston <sally.a.kingston@gmail.com>
2015-11-13 17:01:38 -0500

    finished repo and profile styling

--- main.js
+++ main.js
@@ -1,169 +1,66 @@
-var name = profile.name;
-var username = profile.login;
-var avatarURL = profile.avatar_url;
-var company = profile.company;
-var myLocation = profile.location;
-var email = "<a href = \"mailto:"+ profile.email+ "\">" + profile.email + "</a>";
-var blog = "<a href = \""+ profile.blog+ "\">" + profile.blog + "</a>";
-var joinedDate = moment(profile.created_at).format("MMM DD, YYYY");
-var followers = profile.followers;
-var following = profile.following;
-var starredNum = starred.length;
+var repoTemplate = _.template($("#repoTmpl").html());
+var orgTemplate = _.template($("#orgTmpl").html());
+var pushEventtemplate = _.template($("#pushEventTmpl").html());
+var createRepoEventtemplate = _.template($("#createRepoEventTmpl").html());
+var createBranchEventtemplate = _.template($("#createBranchEventTmpl").html());
 
 
 //Fill header info
-$("title").text(username + " (" + name + ")");
-$(".small-avatar").html("<img src = " + avatarURL + "/>");
+$("title").text(profile.login + " (" + profile.name + ")");
+$(".small-avatar").html("<img src = " + profile.avatar_url + "/>");
 
 //Left sidebar
-$(".profile").prepend("<img src = " + avatarURL + "/>");
-$(".username").text(username);
-$(".name").text(name);
-$(".company").html("<span class=\"octicon octicon-organization\"></span>"+company);
-$(".location").html("<span class=\"octicon octicon-location\"></span>"+myLocation);
-$(".email").html("<span class=\"octicon octicon-mail\"></span>"+email);
-$(".blog").html("<span class=\"octicon octicon-link\"></span>"+blog);
-$(".joinDate").html("<span class=\"octicon octicon-clock\"></span>"+joinedDate);
+$(".profile").prepend("<img src = " + profile.avatar_url + "/>");
+$(".username").text(profile.login);
+$(".name").text(profile.name);
+$(".company").html("<span class=\"octicon octicon-organization\"></span>"+profile.company);
+$(".location").html("<span class=\"octicon octicon-location\"></span>"+profile.location);
+$(".email").html("<span class=\"octicon octicon-mail\"></span><a href = \"mailto:"+ profile.email+ "\">" + profile.email + "</a>");
+$(".blog").html("<span class=\"octicon octicon-link\"></span><a href = \""+ profile.blog+ "\">" + profile.blog + "</a>");
+$(".joinDate").html("<span class=\"octicon octicon-clock\"></span>"+"Joined on "+moment(profile.created_at).format("MMM DD, YYYY"));
+
+
 
 //profile stats:
-$(".stats").html("<a class = \"statLink\" href = "
-                 + profile.html_url
-                 + "/followers >"
-                 + followers
-                 + "Followers</a><a class = \"statLink\" href = https://github.com/stars/"
-                 + profile.login
-                 + ">"
-                 + starredNum
-                 + "Stars</a><a class = \"statLink\" href = "
-                 + profile.html_url
-                 + "/following >"
-                 + following
-                 + "Following</a>"
-               );
+//followers url
+var followers_html="<a class = \"statLink\" href = " + profile.html_url + "/followers ><span class=\"stat\">"+profile.followers +"</span>Followers</a>";
+var starred_html="<a class = \"statLink\" href = https://github.com/stars/" + profile.login + "><span class=\"stat\">" + starred.length + "</span>Stars</a>";
+var following_html="<a class = \"statLink\" href = " + profile.html_url + "/following ><span class=\"stat\">" + profile.following + "</span>Following</a>";
+$(".stats").html(followers_html+starred_html+following_html);
 
- //profile orgs:
- var orgText=""
-
- _.each(orgs, function(org){
-   orgText += "<a href = \"http://github.com/"
-           + org.login
-           + "\"><img src = \""
-           + org.avatar_url
-           + "\"></a>";
- });
-
- $(".orgs").html(orgText);
-
+//profile orgs:
+_.each(orgs, function(org){
+ $(".orgs").append(orgTemplate(org));
+});
 
 //sort repos by date so the newest is first
-repos = _.sortBy(repos,function(repo){
+sortedRepos = _.sortBy(repos,function(repo){
   var dt = new Date(repo.updated_at);
   return -dt;
 });
 
 //add all the repos
-_.each(repos, function (repo) {
-  $(".repos").append("<div class = \"repo\"><h3 class = \"repo_title\"> <a href= \""
-                      + repo.html_url
-                      + "\">"
-                      + repo.name
-                      + "</a></h3><p class = \"repo_data\">Updated "
-                      + moment(repo.updated_at).fromNow()
-                      + "</p><div class = \"repo_stats\">"
-                      + repo.language
-                      + "<a href= \""
-                      + repo.html_url
-                      + "/stargazers\"><span class=\"octicon octicon-star\"></span>"
-                      + repo.stargazers_count
-                      + "</a><a href= \""
-                      + repo.html_url
-                      + "/network\"><span class=\"octicon octicon-git-branch\"></span>"
-                      + repo.forks_count
-                      + "</a></div>");
+var repoFilter = "all";
+_.each(sortedRepos, function (repo) {
+  $("#repos").append(repoTemplate(repo));
 });
 
-var activityText="";
 //for each event
 _.each(events, function (currEvent) {
-  var usernameLink = "<a href=\"http://github.com/" + currEvent.actor.login + "\">" + currEvent.actor.login +"<a/>";
-  var repoLink =  "<a href=\"http://github.com/" + currEvent.repo.name + "\">"  + currEvent.repo.name+"<a/>";
-
   //check to see if it is a push event
   if(currEvent.type==="PushEvent"){
-    var branchLink = "<a href=\"http://github.com/"
-                      + currEvent.repo.name
-                      + "/tree/"
-                      + currEvent.payload.ref.split("/")[2]
-                      + "\">"
-                      + currEvent.payload.ref.split("/")[2]
-                      + "<a/>";
-    //if so, add the CSS with the push event info
-    activityText = "<span class=\"mega-octicon octicon-git-commit\"></span><div class = \"time\">"
-                    + moment(currEvent.created_at).fromNow()
-                    + "</div><div class = \"title\">"
-                    + usernameLink
-                    + " pushed to "
-                    + branchLink
-                    + " at "
-                    + repoLink
-                    + "</div>"
-                    + "<div class = \"details\">"
-                    //+ big avatar
-                    //+ little avatar
-                    + "<a href = \"http://github.com/"
-                    + currEvent.repo.name
-                    + "/commit/"
-                    + currEvent.payload.head
-                    + "\">"
-                    + currEvent.payload.head.substring(0,6)
-                    + "</a> "
-                    + currEvent.payload.commits[0].message;
-    //if not a push event, then it is a create event, and check to see if it was a create repo event
+    $("#activity").append(pushEventtemplate(currEvent));
+  //if not a push event, then it is a create event, and check to see if it was a create repo event
   }else if (currEvent.payload.ref_type === "repository"){
-    //if so, add the CSS with the create repo event info
-    activityText = "<span class=\"octicon octicon-repo\"></span>"
-                    + usernameLink
-                    + " created repository "
-                    + repoLink
-                    + moment(currEvent.created_at).fromNow()
-                    +"<br>";
+    $("#activity").append(createRepoEventtemplate(currEvent));
   }else{
-    var branchLink = "<a href=\"http://github.com/"
-                      + currEvent.repo.name
-                      + "/tree/"
-                      + currEvent.payload.ref
-                      + "\">"
-                      + currEvent.payload.ref
-                      +"<a/>";
-    //if not, it is a create branch event. add the appropriate css and html
-    activityText = "<span class=\"octicon octicon-git-branch\"></span>"
-                    + usernameLink
-                    + " created branch "
-                    + branchLink
-                    + " at "
-                    + repoLink
-                    + moment(currEvent.created_at).fromNow()
-                    +"<br>";
+    //if not, it is a create branch event
+    $("#activity").append(createBranchEventtemplate(currEvent));
   }
-  //put the event info in the activity div
-  $(".activity").append(activityText);
 });
 
-//when you click the repo tab, show the repo info and hide the contributions and activity info
-$(".repoTab").click(function (event) {
-  event.preventDefault();
-  $(".repos").css("display","block");
-  $(".repos").siblings().css("display","none");
-});
-//when you click the contributions tab, show the contributions info and hide the repo and activity info
-$(".contrTab").click(function (event) {
-  event.preventDefault();
-  $(".contributions").css("display","block");
-  $(".contributions").siblings().css("display","none");
-});
-//when you click the activity tab, show the activity info and hide the repo and contributions info
-$(".activTab").click(function (event) {
-  event.preventDefault();
-  $(".activity").css("display","block");
-  $(".activity").siblings().css("display","none");
-});
+//when you click a section tab, show that section and hide the others
+$(".nav-tabs a").click(function (event) {
+    event.preventDefault()
+    $(this).tab('show')
+  });
